2687bfca8efaa5e30dccd4225f0dce1e09a0f3e6
Abbas Mokhtari <49838027+abbasmokhtari@users.noreply.github.com>
2020-05-31 16:05:56 +0100

    SETSTATE

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 let CaughtPokemon = (props) => {
+
+	const [totalCaught, setTotalCaught] = useState(0);
+
+	const incrementTotal = () => {
+		setTotalCaught(totalCaught + 1);
+	}
 	
-	return <p>Caught 0 Pokemon on {props.date}</p>;
+	return (
+		<div>
+			<p>Caught {totalCaught} Pokemon on {props.date}</p>
+			<button onClick={incrementTotal}>Increment</button>
+		</div>
+	)
 };
 
 export default CaughtPokemon;
